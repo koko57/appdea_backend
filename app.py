719c46fdbffc0d7a6ad8f5de15666161b19ffefb
Agata Kosior <agata.kosior57@gmail.com>
2020-08-07 15:14:43 +0200

    Minor changes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def create_app(test_config=None):
 
     return flask_app
 
-app = create_app()
+APP = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run(host='0.0.0.0', port=8080, debug=True)
